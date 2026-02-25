9a28585a8a987d549f781d244b4b4f61e06b09d5
manuel-coyoc <mcoyoc.25934@umsa.edu.mx>
2026-02-24 19:10:08 -0600

    creacion de prefil

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
-
+// Cambio para subir, creacion de pelfil 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
